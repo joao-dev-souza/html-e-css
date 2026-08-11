3ad195005d5abc1b97949f876adbdd2b0279e773
joao-dev-souza <joao.dev38@gmail.com>
2026-08-11 03:51:14 -0300

    Update cadastro.php

--- exercicios/ex025/cadastro.php
+++ exercicios/ex025/cadastro.php
@@ -1,2 +1,21 @@
-os dados foram enviados
-termine com php agora
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro</title>
+</head>
+<body>
+    <h1>Está tudo correto?</h1>
+    <tbody>
+        <tr>
+            <th>
+                Nome completo:
+            </th>
+            <td>
+                <?= "$nome $sobrenome" ?>
+            </td>
+        </tr>
+    </tbody>
+</body>
+</html>
